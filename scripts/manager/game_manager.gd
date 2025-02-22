aee702f3adc0ec063b9151ff6742073c522e7af4
Kauan <kauanmonteirosilva789@gmail.com>
2025-02-22 11:47:10 -0300

    Implementado o sistema de checkpoint.

--- scripts/manager/game_manager.gd
+++ scripts/manager/game_manager.gd
@@ -1,4 +1,4 @@
-extends Node
+extends Node2D
 
 var puzzle1 = false
 var puzzle2 = false
@@ -7,3 +7,16 @@ var puzzle3 = false
 var die_fire_slime = false
 
 var from_scene
+
+var last_position = null
+
+# No script Checkpoint
+func respawn_at_checkpoint():
+	if last_position:
+		# Altere a posição do checkpoint e adie a reinicialização da cena
+		global_position = last_position
+		call_deferred("_reload_current_scene")
+
+func _reload_current_scene():
+	get_tree().reload_current_scene()
+	
